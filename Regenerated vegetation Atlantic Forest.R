7cd161ad88792e2dad363648851fcb1294e56991
Vinicius Tonetti <vrtonetti@gmail.com>
2025-02-15 20:02:48 -0300

    commenting and loading raster

--- Regenerated vegetation Atlantic Forest.R
+++ Regenerated vegetation Atlantic Forest.R
@@ -7,5 +7,13 @@ library(terra)
 library(sf)
 
 # Municipalities
-
+# Shapefile downloaded from https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/15774-malhas.html on 15/02/2025
 mun <- terra::vect("D:/__PESSOAL/Vinicius_T/municipios_Brasil/BR_Municipios_2023/BR_Municipios_2023.shp")
+
+
+
+# Regeneration 11_21
+reg_11_21 <- terra::rast("D:/__PESSOAL/Vinicius_T/raster_pacto/_reg_11_21.tif")
+
+# Projecting municipalities CRS to the same CRS in raster
+mun <- project(mun, crs(reg_11_21))
