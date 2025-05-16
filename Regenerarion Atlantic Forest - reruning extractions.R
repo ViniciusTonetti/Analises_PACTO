92c36ef3a24b48ea9669e2df926777dc8f86b99a
Vinicius Tonetti <vrtonetti@gmail.com>
2025-05-15 23:26:09 -0300

    cropping annual raster

--- Regenerarion Atlantic Forest - reruning extractions.R
+++ Regenerarion Atlantic Forest - reruning extractions.R
@@ -26,6 +26,8 @@ st_write(mun_AF_WGS, "D:/__PESSOAL/Vinicius_T/municipios_Brasil/BR_Municipios_20
 
 ## Cropping Annual reg raster --------------------------------------------------
 
+AF <- vect("D:/__PESSOAL/Vinicius_T/Limite Mata Atlantica/bioma_MA_IBGE_250mil/bioma_MA_IBGE_250mil.shp")
+
 annual_rast <- rast(c("D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen11_1ha.tif",
                       "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen12_1ha.tif",
                       "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen13_1ha.tif",
@@ -37,6 +39,19 @@ annual_rast <- rast(c("D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pa
                       "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen19_1ha.tif",
                       "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen20_1ha.tif",
                       "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/regen21_1ha.tif"))
+
+dir <- "D:/__PESSOAL/Vinicius_T/raster_pacto/Tiles Reg 11 - 20 Pacto-20250308T211602Z-001/Tiles Reg 11 - 20 Pacto/"
+
+obj_name <- paste0("annual_reg_", 11:21)
+
+for (i in 1:length(names(annual_rast))) {
+  raster <- mask(crop(annual_rast[[i]], AF), AF)
+  terra::writeRaster(raster, paste(dir, obj_name[i], "AF.tif", sep = ""),
+                     gdal=c("COMPRESS=DEFLATE", "TFW=YES"), overwrite = T)
+}
+
+
+
 
 
 
