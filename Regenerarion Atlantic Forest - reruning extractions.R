7e0e1f59e1a8bf218e6656626aa68723ef8198a2
Vinicius Tonetti <vrtonetti@gmail.com>
2025-05-15 22:25:34 -0300

    Mun intersects AF

--- Regenerarion Atlantic Forest - reruning extractions.R
+++ Regenerarion Atlantic Forest - reruning extractions.R
@@ -1,2 +1,22 @@
 # Code used to rerun the extraction 
 # Vinicius Tonetti - vrtonetti@ufscar.br
+
+# packages
+
+library(terra)
+library(sf)
+
+# cleaning directory 
+rm(list = ls())
+
+# Considering only Atlantic Forest municipalities
+
+AF <- st_read("D:/__PESSOAL/Vinicius_T/Limite Mata Atlantica/bioma_MA_IBGE_250mil/bioma_MA_IBGE_250mil.shp")
+mun <- st_read("D:/__PESSOAL/Vinicius_T/municipios_Brasil/BR_Municipios_2023/BR_Municipios_2023.shp")
+mun_WGS <- st_transform(mun, st_crs(AF))
+
+# Municipalities that intersect AF 
+
+mun_AF <- mun_WGS[st_intersects(mun_WGS, AF, sparse = F), ]
+
+
